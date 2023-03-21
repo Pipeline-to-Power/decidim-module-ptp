082cdfe047d10a0dca347efc782ed8ac84ad9814
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-03-21 12:07:37 +0200

    Remove authorization before action and selected_projet helper methods

--- decidim-budgets_booth/app/controllers/decidim/budgets/voting_controller.rb
+++ decidim-budgets_booth/app/controllers/decidim/budgets/voting_controller.rb
@@ -5,8 +5,9 @@ module Decidim
     class VotingController < ProjectsController
       include Decidim::Budgets::NeedsCurrentOrder
       include Decidim::UserProfile
+      include ::Decidim::BudgetsBooth::BudgetsControllerHelper
 
-      layout :determine_layout
+      layout "decidim/budgets/voting_layout"
       before_action :store_user_location!, if: :storable_location?
       # The callback which stores the current location must be added before you authenticate the user
       # as `authenticate_user!` (or whatever your resource is) will halt the filter chain and redirect
@@ -14,16 +15,13 @@ module Decidim
 
       before_action :authenticate_user!
       before_action :ensure_voting_open!
-      before_action :ensure_authorized!, if: :authorization_required?
+      before_action :ensure_authenticated, if: :zip_code_workflow?
+      before_action :ensre_user_zip_code, if: :zip_code_workflow?
       before_action :ensure_not_voted!, only: [:index]
 
       def index
         enforce_permission_to :vote, :project, project: budget.projects.first, budget: budget, workflow: current_workflow
-        @select_project = set_selected_project
-        unset_select_params
       end
-
-      def confirm; end
 
       def user_has_no_permission
         flash[:alert] = if current_user
@@ -65,71 +63,16 @@ module Decidim
         redirect_to decidim_budgets.budget_projects_path(budget)
       end
 
-      def ensure_authorized!
-        return if user_authorized?
-
-        if authorization_options.one?
-          # If the authorization option is one, it is sms authorization, since
-          # this authorization should be selected if configured.
-          flash[:warning] = I18n.t("decidim.budgets.voting.general.not_authorized")
-          authorization_method = Decidim::Verifications::Adapter.from_element(authorization_options.first)
-          redirect_to authorization_method.root_path(redirect_url: decidim_budgets.budget_voting_index_path(budget))
-        else
-          flash[:warning] = I18n.t("decidim.budgets.voting.general.multi_authorization_warning")
-          @auth_options = authorization_methods
-
-          render :auth_methods
-        end
-      end
-
       def ensure_not_voted!
-        redirect_to decidim_budgets.confirm_budget_voting_index_path(budget) if current_order.checked_out?
+        redirect_to decidim_budgets.budgets_path if current_order.checked_out?
       end
 
       def authorization_required?
         @authorization_required ||= authorization_options&.present?
       end
 
-      def authorization_options
-        permission = current_component.permissions&.fetch("vote", nil)
-        permission&.fetch("authorization_handlers", nil)&.keys
-      end
-
-      # This ensures that only people eligible to vote can enter the voting
-      #  after the authorization step. The authorization conditions
-      # should be used to control user's ability to vote (e.g. where they live,
-      # how old they are, etc.).
-      def user_authorized?
-        @user_authorized ||= user_signed_in? && action_authorized_to("vote").ok?
-      end
-
-      def authorization_methods
-        @authorization_methods ||= available_verification_workflows.select do |handler|
-          authorization_options.include?(handler.key)
-        end
-      end
-
-      def set_selected_project
-        return if params[:select_project].blank?
-
-        project = budget&.projects&.find(params[:select_project])
-        return unless project && projects.exclude?(project)
-
-        project
-      end
-
-      def unset_select_params
-        params[:select_project] = nil
-      end
-
       def decidim_budgets
         @decidim_budgets ||= Decidim::EngineRouter.main_proxy(current_component)
-      end
-
-      def determine_layout
-        return nil if @auth_options.present?
-
-        "decidim/budgets/voting_layout"
       end
     end
   end
