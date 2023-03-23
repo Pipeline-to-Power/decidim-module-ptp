cee7a7d1e3a0d8a5cef078857ad03714264d2224
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-03-23 15:48:14 +0200

    Show budgets list also  when the voting is not open

--- decidim-budgets_booth/app/controllers/concerns/decidim/budgets_booth/budgets_controller_extensions.rb
+++ decidim-budgets_booth/app/controllers/concerns/decidim/budgets_booth/budgets_controller_extensions.rb
@@ -8,7 +8,7 @@ module Decidim
 
       included do
         layout :determine_layout
-        before_action :ensure_authenticated, if: :zip_code_workflow?
+        before_action :ensure_authenticated, if: :zip_code_workflow? && :voting_open?
         before_action :ensre_user_zip_code, if: :zip_code_workflow? && :voting_open?
 
         def index
