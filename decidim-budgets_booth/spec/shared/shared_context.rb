a9f2b8fccf8f545749fff32b2c5dfc53f0de3fd8
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-03-31 16:19:15 +0300

    Add shared contexts for user_zip_code and scopes

--- decidim-budgets_booth/spec/shared/shared_context.rb
+++ decidim-budgets_booth/spec/shared/shared_context.rb
@@ -1,27 +1,24 @@
 # frozen_string_literal: true
 
-RSpec.shared_context "with single budget" do
-  let(:budget) { create(:budget, component: component, total_budget: 100_000) }
-end
-
-RSpec.shared_context "with multiple budgets" do
-  include_context "with single budget"
-  let(:another_budget) { create(:budget, component: component, total_budget: 150_000) }
-end
-
-RSpec.shared_context "with single permission" do
-  let(:available_authorizations) { %w(dummy_authorization_handler another_dumy_authorization_handler) }
-  let(:permissions) do
-    {
-      vote: {
-        authorization_handlers: {
-          "dummy_authorization_handler" => {}
-        }
-      }
-    }
-  end
+RSpec.shared_context "with scopes" do
+  let!(:parent_scopes) { create_list(:scope, 2, organization: organization) }
+  let!(:subscopes) { create_list(:scope, 2, parent: parent_scopes.second) }
+  let!(:first_postals) { create_list(:scope, 5, parent: parent_scopes.first) }
+  let!(:second_postals) { create_list(:scope, 7, parent: subscopes[0]) }
+  let!(:third_postals) { create_list(:scope, 8, parent: subscopes[1]) }
   before do
-    component.organization.update!(available_authorizations: available_authorizations)
-    component.update!(permissions: permissions)
+    first_postals.each_with_index do |postal, i|
+      postal.update(code: "#{postal.code}_#{i + 1000}")
+    end
+    second_postals.each_with_index do |postal, i|
+      postal.update(code: "#{postal.code}_#{i + 1004}")
+    end
+    third_postals.each_with_index do |postal, i|
+      postal.update(code: "#{postal.code}_#{i + 1010}")
+    end
   end
+end
+
+RSpec.shared_context "with user_data" do
+  let!(:user_data) { create(:user_data, component: component, user: user) }
 end
