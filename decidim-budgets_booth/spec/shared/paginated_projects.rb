f6634d8e0db1c20b0860ed25ad9b849ea88e0544
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-04-06 14:48:09 +0300

    Move  shared examples to the voting booth specs

--- decidim-budgets_booth/spec/shared/paginated_projects.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-shared_examples "paginated projects" do
-  it "paginates the projects" do
-    expect(page).to have_css(".budget-list .budget-list__item", count: 5)
-    find("li.page", text: "2").click
-    expect(page).to have_css(".budget-list .budget-list__item", count: 5)
-  end
-end
