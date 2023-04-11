0d3f8c5277c1a6fec37d4dc21165f39fd7dee994
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-04-11 12:19:19 +0300

    render zip code inputs dynamically based on zip_code_length config

--- decidim-budgets_booth/app/helpers/decidim/budgets/user_data_helper.rb
+++ decidim-budgets_booth/app/helpers/decidim/budgets/user_data_helper.rb
@@ -6,6 +6,10 @@ module Decidim
       def voting_terms
         translated_attribute(component_settings.try(:voting_terms)).presence
       end
+
+      def zip_code_length
+        ::Decidim::BudgetsBooth.zip_code_length.to_i
+      end
     end
   end
 end
