74767e78c9f262bfb90d856e96e846e29e416f2c
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-04-12 15:51:34 +0300

    Complete javascript for zip code entering

--- decidim-budgets_booth/app/packs/src/decidim/budgets_booth/zip_coder.js
+++ decidim-budgets_booth/app/packs/src/decidim/budgets_booth/zip_coder.js
@@ -1,37 +1,104 @@
 $(() => {
   const zipcodeInputs = document.querySelectorAll('#zip-code input[type="text"]');
-  const zipCodeelement = document.querySelector("#zip-code-not-valid");
-  const togglevalidity = (item) => {
-    if (item.classList.contains("is-invisible")) {
-      item.classList.remove("is-invisible")
-      item.classList.add("is-visible")
-    } else {
-      item.classList.remove("is-visible")
-      item.classList.add("is-invisible")
+  const zipCodeNotValid = document.querySelector("#zip-code-not-valid");
+  const zipCodeError = document.querySelector("#zip-code-error");
+  const onlyLettersAllowed = document.querySelector("#only-letters-allowed");
+
+  const removeAllErrorMessages = () => {
+    document.querySelectorAll(".form-error").forEach((element) => {
+      if (element.classList.contains("is-visible")) {
+        element.classList.remove("is-visible")
+        element.classList.add("is-invisible")
+      }
+    })
+  }
+
+  const showErrorMessage = (element) => {
+    removeAllErrorMessages()
+    if (element.classList.contains("is-invisible")) {
+      element.classList.remove("is-invisible")
+      element.classList.add("is-visible")
     }
   }
-  if (zipCodeelement.dataset.invalid) {
-    console.log("The valididity is", zipCodeelement.dataset.invalid);
+  const invalidField = (element) => {
+    zipcodeInputs.forEach((item) => {
+      if (!item.classList.contains("is-invalid-input")) {
+        item.classList.add("is-invalid-input")
+      }
+    })
+    showErrorMessage(element)
+  }
+
+  const resetField = () => {
+    zipcodeInputs.forEach((item) => {
+      if (item.classList.contains("is-invalid-input")) {
+        item.classList.remove("is-invalid-input")
+      }
+    })
+  }
+
+  const validUserInput = (val) => (/^[a-zA-Z0-9]+$/).test(val)
+
+  if (zipCodeNotValid.dataset.invalid) {
     zipcodeInputs.forEach((input) => {
       input.classList.add("is-invalid-input")
     });
-    togglevalidity(zipCodeelement)
+    invalidField(zipCodeNotValid)
   }
   zipcodeInputs.forEach((input, ind) => {
     input.setAttribute("maxlength", "1");
     input.addEventListener("click", () => {
+      resetField();
       input.select();
     })
+
+    input.addEventListener("paste", (event) => {
+      const clipboardData = event.clipboardData || window.clipboardData;
+      const pastedData = clipboardData.getData("text").trim();
+      if (!validUserInput(pastedData)) {
+        invalidField(onlyLettersAllowed)
+        input.blur();
+        input.classList.add("is-invalid-input");
+        return
+      }
+
+      // find the first empty input field and paste the data there
+      let jj = 0;
+      for (let ii = ind; ii < zipcodeInputs.length; ii += 1) {
+        if (jj > pastedData.length) {
+          console.log("1")
+          return;
+        }
+        if (pastedData.substr(jj, 1)) {
+          zipcodeInputs[ii].value = pastedData.substr(jj, 1);
+          zipcodeInputs[ii].focus();
+          jj += 1
+        }
+      }
+      event.preventDefault();
+    });
+    input.addEventListener("keydown", function(event) {
+      const previousInput = zipcodeInputs[ind - 1];
+      if (event.key === "Backspace") {
+        if (previousInput) {
+          previousInput.select();
+          event.preventDefault();
+          input.value = ""
+        }
+        console.log("Backspace key pressed");
+      }
+    });
+
     input.addEventListener("input", () => {
       const val = input.value
 
-      if (!(/\d/).test(val)) {
+      if (!validUserInput(val)) {
         input.value = ""
         return
       }
       const nextInput = zipcodeInputs[ind + 1];
       if (nextInput) {
-        nextInput.focus();
+        nextInput.select();
       } else {
         input.blur()
       }
@@ -55,21 +122,15 @@ $(() => {
     document.querySelector('input[name="user_data[metadata]"]').value = combinedValue
   };
 
-  const makeFieldsInvalid = () => {
-    zipcodeInputs.forEach((input) => {
-      input.classList.add("is-invalid-input")
-    })
-    togglevalidity(document.querySelector("#zip-code-error"))
-  }
-
   const form = document.querySelector(".new_user_data");
   $(form).on("submit", (ev) => {
+    removeAllErrorMessages()
     setZipcodeField();
     if (fieldsvalid()) {
       return;
     }
 
     ev.preventDefault();
-    makeFieldsInvalid();
+    invalidField(zipCodeError);
   });
 })
