56e0eb7ad275739cc1fbbf37f3a9313f5628cee8
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-04-13 11:52:38 +0300

    Add cancel link to the user_data view with dynamic redirect

--- decidim-budgets_booth/app/helpers/decidim/budgets/user_data_helper.rb
+++ decidim-budgets_booth/app/helpers/decidim/budgets/user_data_helper.rb
@@ -3,12 +3,32 @@
 module Decidim
   module Budgets
     module UserDataHelper
+      delegate :user_zip_code, to: :scope_manager
+
       def voting_terms
         translated_attribute(component_settings.try(:voting_terms)).presence
       end
 
       def zip_code_length
         ::Decidim::BudgetsBooth.zip_code_length.to_i
+      end
+
+      # we need to redirect user to the root path if they
+      # want to candel the user_data entering, and they don't have a zip code. otherwise we
+      # get into infinit redirect loop, since the budgets index path redirects users
+      # to this view if they dont have a zip code.
+      def cancel_redirect_path
+        if user_zip_code(current_user, current_component)
+          decidim_budgets.budgets_path
+        else
+          decidim.root_path
+        end
+      end
+
+      private
+
+      def scope_manager
+        @scope_manager ||= ::Decidim::BudgetsBooth::ScopeManager.new
       end
     end
   end
