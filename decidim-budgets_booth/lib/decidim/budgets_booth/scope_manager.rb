8ab5aa0418801cf940e8d5367f2393a5da5c8f31
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-04-24 11:26:26 +0300

    Add local caching to scopes manager to avoid multiple calls
    In order to fetch the scopes more quickly for the different scope
    manager instances, store them into local cache.

--- decidim-budgets_booth/lib/decidim/budgets_booth/scope_manager.rb
+++ decidim-budgets_booth/lib/decidim/budgets_booth/scope_manager.rb
@@ -5,6 +5,93 @@ module Decidim
     class ScopeManager
       attr_reader :component, :top_scope
 
+      class << self
+        # This method allows storing the scopes mappings locally so that they do
+        # not have to be re-fetched for the multiple instances of the
+        # ScopeManager class.
+        def scopes_mapping_for(scope)
+          scopes_mapping_cache[scope.id] ||= generate_scopes_mapping_for(scope)
+        end
+
+        # Allow clearing the cache, useful for the specs.
+        def clear_cache!
+          @scopes_mapping_cache = {}
+        end
+
+        private
+
+        # Stores the local cache of the scopes mappings.
+        def scopes_mapping_cache
+          @scopes_mapping_cache ||= {}
+        end
+
+        # Generates the scopes mapping for the given top-level scope.
+        def generate_scopes_mapping_for(scope)
+          locale = scope.organization.default_locale
+          table = Decidim::Scope.table_name
+          connection = ActiveRecord::Base.connection
+          columns = "id, parent_id, code, name->>#{connection.quote(locale)} AS name"
+          topquery = "SELECT %columns% FROM #{table} WHERE parent_id = #{connection.quote(scope.id)}"
+          queries = []
+
+          # Maximum of 3 levels below the top scope:
+          #   Boroughs -> Neighborhoods -> Postal codes
+          #
+          # The postal codes are defined at the deepest level regardless of the
+          # amount of levels.
+          subquery = topquery
+          3.times do |i|
+            queries << subquery.sub("%columns%", "#{columns}, #{i} AS depth")
+            subquery = "SELECT %columns% FROM #{table} WHERE parent_id IN (#{subquery.sub("%columns%", "id")})"
+          end
+
+          # Query all the levels and store the postal code mapping. Note that
+          # the order by depth is important for the further processing. The
+          # lowest depth needs to be processed first.
+          result = connection.select_all("#{queries.join(" UNION ALL ")} ORDER BY depth, name").to_a
+
+          zip_codes_hash(scope, result)
+        end
+
+        # Converts the multi-level query results into a flat hash which has the
+        # scope IDs as keys and their related ZIP codes as values. This approach
+        # allows quickly fetching the ZIP codes for each scope.
+        def zip_codes_hash(parent_scope, result)
+          max_depth = result.pluck("depth").max
+          parents = { parent_scope.id => [] }
+
+          {}.tap do |mapping|
+            result.each do |item|
+              if item["depth"] == max_depth
+                # Postal code
+                mapping[item["parent_id"]] ||= []
+                mapping[item["parent_id"]] << item["name"]
+
+                each_item(parents, item["parent_id"]) do |parent_id|
+                  mapping[parent_id] ||= []
+                  mapping[parent_id] << item["name"]
+                end
+              elsif item["depth"].positive?
+                parents[item["id"]] ||= []
+                parents[item["id"]] << item["parent_id"] unless parents[item["id"]].include?(item["parent_id"])
+
+                each_item(parents, item["parent_id"]) do |parent_id|
+                  parents[item["id"]] << parent_id unless parents[item["id"]].include?(parent_id)
+                end
+              end
+            end
+          end
+        end
+
+        # This is a helper method to reduce the cyclomatic complexity of the
+        # `zip_codes_hash` method.
+        def each_item(parents, id)
+          return unless parents[id]
+
+          parents[id].each { |parent_id| yield parent_id }
+        end
+      end
+
       def initialize(component)
         @component = component
         @top_scope = component.scope
@@ -36,70 +123,7 @@ module Decidim
       end
 
       def scopes_mapping
-        @scopes_mapping ||= begin
-          locale = top_scope.organization.default_locale
-          table = Decidim::Scope.table_name
-          connection = ActiveRecord::Base.connection
-          columns = "id, parent_id, code, name->>#{connection.quote(locale)} AS name"
-          topquery = "SELECT %columns% FROM #{table} WHERE parent_id = #{connection.quote(top_scope.id)}"
-          queries = []
-
-          # Maximum of 3 levels below the top scope:
-          #   Boroughs -> Neighborhoods -> Postal codes
-          #
-          # The postal codes are defined at the deepest level regardless of the
-          # amount of levels.
-          subquery = topquery
-          3.times do |i|
-            queries << subquery.sub("%columns%", "#{columns}, #{i} AS depth")
-            subquery = "SELECT %columns% FROM #{table} WHERE parent_id IN (#{subquery.sub("%columns%", "id")})"
-          end
-
-          # Query all the levels and store the postal code mapping. Note that
-          # the order by depth is important for the further processing. The
-          # lowest depth needs to be processed first.
-          result = connection.select_all("#{queries.join(" UNION ALL ")} ORDER BY depth, name").to_a
-
-          zip_codes_hash(result)
-        end
-      end
-
-      # Converts the multi-level query results into a flat hash which has the
-      # scope IDs as keys and their related ZIP codes as values. This approach
-      # allows quickly fetching the ZIP codes for each scope.
-      def zip_codes_hash(result)
-        max_depth = result.pluck("depth").max
-        parents = { top_scope.id => [] }
-
-        {}.tap do |mapping|
-          result.each do |item|
-            if item["depth"] == max_depth
-              # Postal code
-              mapping[item["parent_id"]] ||= []
-              mapping[item["parent_id"]] << item["name"]
-
-              each_item(parents, item["parent_id"]) do |parent_id|
-                mapping[parent_id] ||= []
-                mapping[parent_id] << item["name"]
-              end
-            elsif item["depth"].positive?
-              parents[item["id"]] ||= []
-              parents[item["id"]] << item["parent_id"] unless parents[item["id"]].include?(item["parent_id"])
-
-              each_item(parents, item["parent_id"]) do |parent_id|
-                parents[item["id"]] << parent_id unless parents[item["id"]].include?(parent_id)
-              end
-            end
-          end
-        end
-      end
-
-      # This is a helper method to reduce the cyclomatic complexity of the
-      # `zip_codes_hash` method.
-      def each_item(parents, id)
-        return unless parents[id]
-
-        parents[id].each { |parent_id| yield parent_id }
+        self.class.scopes_mapping_for(top_scope)
       end
     end
   end
