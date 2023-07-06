1513d5ae489a48634b1db415c737f1be7195c2a6
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-07-06 17:04:43 +0300

    Fix failing tests

--- decidim-smsauth/app/commands/decidim/smsauth/send_verification_code.rb
+++ decidim-smsauth/app/commands/decidim/smsauth/send_verification_code.rb
@@ -54,7 +54,7 @@ module Decidim
       end
 
       def generate_code
-        code = SecureRandom.random_number(10**@auth_code_length).to_s
+        code = SecureRandom.random_number(10**auth_code_length).to_s
         add_zeros(code)
       end
 
@@ -67,9 +67,9 @@ module Decidim
       end
 
       def add_zeros(code)
-        return code if code.length == @auth_code_length
+        return code if code.length == auth_code_length
 
-        ("0" * (@auth_code_length - code.length)) + code
+        ("0" * (auth_code_length - code.length)) + code
       end
     end
   end
